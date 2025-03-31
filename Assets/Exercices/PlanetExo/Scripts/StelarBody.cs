abfb0f78e3a53a309ec96e3af72705af609de377
Luoma <vokinne@gmail.com>
2025-03-31 16:16:22 +0200

    add moon rotation

--- Assets/Exercices/PlanetExo/Scripts/StelarBody.cs
+++ Assets/Exercices/PlanetExo/Scripts/StelarBody.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StelarBody : MonoBehaviour
 {
     [SerializeField] float range = 1.0f;
     [SerializeField] float moveSpeed = 1.0f;
     [SerializeField] float rotateSpeed = 1.0f;
-    [SerializeField] bool isRotPos = true;
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool isClockwise = true;
+    [SerializeField] Transform rotateOn;
     void Update()
-    {        
-        transform.position = new Vector3(Mathf.Cos(Time.time * moveSpeed), 0, Mathf.Sin(Time.time * moveSpeed)) * range;
-        transform.Rotate(transform.up, Mathf.Cos(Time.deltaTime * rotateSpeed) * (isRotPos ? 1 : -1), Space.World);
+    {
+        transform.Rotate(transform.up, Mathf.Cos(Time.deltaTime * rotateSpeed) * GetRotationDirection(), Space.World);
+        if (rotateOn == null) return;
+        transform.position = new Vector3(Mathf.Cos(Time.time * moveSpeed), 0, Mathf.Sin(Time.time * moveSpeed)) * range + rotateOn.position;
+    }
+
+    float GetRotationDirection()
+    {
+        return isClockwise ? 1.0f : -1.0f;
     }
 }
