72d424b38cc1b4258a05ca79ff2af0f07ecd6f16
Luoma <vokinne@gmail.com>
2025-04-01 11:14:23 +0200

    add a boolean to stop sim

--- Assets/Exercices/PlanetExo/Scripts/StelarBody.cs
+++ Assets/Exercices/PlanetExo/Scripts/StelarBody.cs
@@ -9,9 +9,11 @@ public class StelarBody : MonoBehaviour
     [SerializeField] float moveSpeed = 1.0f;
     [SerializeField] float rotateSpeed = 1.0f;
     [SerializeField] bool isClockwise = true;
+    [SerializeField] bool stopSimulation = false;
     [SerializeField] Transform rotateOn;
     void Update()
     {
+        if (stopSimulation) return;
         transform.Rotate(transform.up, Mathf.Cos(Time.deltaTime * rotateSpeed) * GetRotationDirection(), Space.World);
         if (rotateOn == null) return;
         transform.position = new Vector3(Mathf.Cos(Time.time * moveSpeed), 0, Mathf.Sin(Time.time * moveSpeed)) * range + rotateOn.position;
