e7a587b832139588db62264a4f031930f4dd0993
Luoma <vokinne@gmail.com>
2025-04-01 12:21:27 +0200

    edit prefab, modify stelarbody script for cam offset

--- Assets/Exercices/PlanetExo/Scripts/StelarBody.cs
+++ Assets/Exercices/PlanetExo/Scripts/StelarBody.cs
@@ -11,16 +11,32 @@ public class StelarBody : MonoBehaviour
     [SerializeField] bool isClockwise = true;
     [SerializeField] bool stopSimulation = false;
     [SerializeField] Transform rotateOn;
+    [SerializeField] Vector3 positionOffset = Vector3.zero;
+    [SerializeField] Vector3 rotationOffset = Vector3.zero;
     void Update()
     {
+        //Camera.main.transform.parent.position = positionOffset;
+        //Camera.main.transform.parent.eulerAngles = rotationOffset;
         if (stopSimulation) return;
         transform.Rotate(transform.up, Mathf.Cos(Time.deltaTime * rotateSpeed) * GetRotationDirection(), Space.World);
         if (rotateOn == null) return;
-        transform.position = new Vector3(Mathf.Cos(Time.time * moveSpeed), 0, Mathf.Sin(Time.time * moveSpeed)) * range + rotateOn.position;
+        transform.position = GetNewPosition();
     }
 
     float GetRotationDirection()
     {
         return isClockwise ? 1.0f : -1.0f;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(positionOffset + GetNewPosition(), 0.2f);
+        //    Gizmos.color = Color.red;
+        //    Gizmos.DrawRay(positionOffset, transform.position);
+    }
+    private Vector3 GetNewPosition()
+    {
+        return new Vector3(Mathf.Cos(Time.time * moveSpeed), 0, Mathf.Sin(Time.time * moveSpeed)) * range + rotateOn.position;
     }
 }
